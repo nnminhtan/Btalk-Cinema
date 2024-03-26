f6ed01e70e4629709a1d99f7548e160ac5ab27f7
CHAUKIENLUONG <luong211003@gmail.com>
2024-03-26 22:29:49 +0700

    add banner and edit navbar

---
+++ WebCinema/wwwroot/js/script.js.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    dropdownMenu.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.icon_profile')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
