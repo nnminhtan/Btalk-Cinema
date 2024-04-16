666811d9ec88d4afddf0519a7cb07c3433e9623c
Hải Bằng <haibang16112003@gmail.com>
2024-04-16 22:51:41 +0700

    movieee

---
+++ WebCinema/Controllers/PhimController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCinema.Repositories;
+
+namespace WebCinema.Controllers
+{
+
+    public class PhimController : Controller
+    {
+        private readonly IGenreRepo _genreRepo;
+        private readonly IMovieRepo _movieRepo;
+
+        public PhimController(IMovieRepo movieRepo, IGenreRepo genreRepo)
+        {
+            _movieRepo = movieRepo;
+            _genreRepo = genreRepo;
+        }
+        public async Task<IActionResult> Movie()
+        {
+            var movies = await _movieRepo.GetAllAsync();
+            return View(movies);
+        }
+
+
+
+    }
+
+}
