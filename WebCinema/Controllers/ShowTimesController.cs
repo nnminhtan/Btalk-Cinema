45f9a759a130fcb04f8d9afe623a6374c0e5ea69
TruongAn <leantruong101203@gmail.com>
2024-04-17 20:27:29 +0700

    fix showtime

--- WebCinema/Controllers/ShowTimesController.cs
+++ WebCinema/Controllers/ShowTimesController.cs
@@ -1,23 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebCinema.Models;
 using WebCinema.Repositories;
 
 namespace WebCinema.Controllers
 {
     public class ShowTimesController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly IGenreRepo _genreRepo;
         private readonly IMovieRepo _movieRepo;
 
-        public ShowTimesController(IMovieRepo movieRepo, IGenreRepo genreRepo)
+        public ShowTimesController(IMovieRepo movieRepo, IGenreRepo genreRepo, ApplicationDbContext context)
         {
+           
             _movieRepo = movieRepo;
             _genreRepo = genreRepo;
+            _context = context;
         }
         public async Task<IActionResult> ShowTimes()
         {
-            //var movies = await _movieRepo.GetAllAsync();
-            //return View(movies);
+
+            var movies = await _movieRepo.GetAllAsync();
+            //return View(movies);                                            
             var moviesWithShowtimes = await _movieRepo.GetAllWithShowtimesAndScreentimesAsync();
             return View(moviesWithShowtimes);
         }
